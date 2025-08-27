eca7f5b250cede7bcc7b1b986294457fece3159c
Albuquerque07 <rafa.moraes2003@hotmail.com>
2025-08-27 17:09:11 -0300

    Update banco_de_dados.py

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -5,5 +5,8 @@ deve retornar um dicionário vazio.
 Retorno: Um dicionário no formato {"numero_conta": {"cliente": "Nome", "saldo": 123.45}, ...}. 
 O saldo deve ser um float."""
 
-
-FALAAAA RAFAAAA
+caminho = "contas.csv"
+def ler_dicionario(dicionario):
+    with open(caminho, "r") as arquivo:
+        arquivo.write()
+    pass
