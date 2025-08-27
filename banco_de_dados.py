010b884a91576b93bf96d3b8aa42b7cf522074eb
LucasRolim2811 <lucasrolim2811@gmail.com>
2025-08-27 16:02:18 -0300

    Estrutura

---
+++ banco_de_dados.py
@@ -0,0 +1,6 @@
+"""Descrição: Lê o arquivo CSV e o carrega em um dicionário. Se o arquivo não existir, 
+deve retornar um dicionário vazio.
+
+
+Retorno: Um dicionário no formato {"numero_conta": {"cliente": "Nome", "saldo": 123.45}, ...}. 
+O saldo deve ser um float."""
