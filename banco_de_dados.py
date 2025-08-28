14641e3e1d83cdcce2999ae66d4595cd8bfa8e17
LucasRolim2811 <lucasrolim2811@gmail.com>
2025-08-28 13:23:31 -0300

    Update banco_de_dados.py

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -7,10 +7,42 @@ O saldo deve ser um float."""
 
 import csv
 
-caminho = "contas.csv" 
+def carregar_contas_de_csv(caminho_arquivo: str) -> dict: #Está funcionando bem
+    """ Faz o acesso ao arquivo csv e lê cada linha contida dentro dele
 
-def ler_dicionario(dicionario):
-	with open (caminho, "r") as arquivo:
-		dicionario = csv.reader(arquivo, delimiter=",", encoding="utf-8")
-		for linha in dicionario:
-			print(linha)
+    Args:
+        caminho_arquivo (str): Argumento que recebe o caminho em que o arquivo csv está
+
+    Returns:
+        dict: Retorna um dicionário possuindo as linhas contidas no arquivo csv dado em caminho_arquivo
+    """
+    with open(caminho_arquivo, "r",encoding='utf-8') as arquivo_csv:
+        acessando_arquivo = csv.reader(arquivo_csv)
+        for lines in acessando_arquivo:
+            print(lines)
+    return
+
+def salvar_contas_para_csv(caminho_arquivo:str="contas.csv", contas:dict=dict()) -> None: #Precisa verificar se está salvando o arquivo no final
+    """Insere um dicionário dado pelo usuário dentro do banco de dados do csv passado pelo mesmo
+
+    Args:
+        caminho_arquivo (str): Argumento que recebe o caminho em que o arquivo csv está
+        contas (dict): Dicionário que o usuário quer inserir no banco de dados do arquivo csv
+    """
+    try:
+        with open(caminho_arquivo, "w", encoding="utf-8", newline="") as arquivo_csv:
+            colunas = ["nome_cliente", "saldo"]
+            acessar_csv = csv.DictWriter(arquivo_csv, fieldnames=colunas)
+            acessar_csv.writeheader()
+            acessar_csv.writerow(contas)
+    except FileNotFoundError:
+        print(f"O arquivo {caminho_arquivo} dado pelo usuário não foi encontrado.")
+    else:
+        print("Arquivo acessado COM SUCESSO!!!")
+
+
+# Driver code
+
+carregar_contas_de_csv("contas.csv")
+salvar_contas_para_csv("contas.csv", {"nome_cliente": "rafael", "saldo":2000})
+carregar_contas_de_csv("contas.csv")
