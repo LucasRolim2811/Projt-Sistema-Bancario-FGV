e808f17fd61a07c7542157eb2213b043bc2ef879
LucasRolim2811 <lucasrolim2811@gmail.com>
2025-08-28 22:41:29 -0300

    Novas Alterações Main

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from time import sleep
-
-print("#" * 30)
-print("Bem vindo ao Banco Fox!")
-print("#" * 30)
-
+import banco_de_dados
+import operacoes_bancarias
 resposta = 0
 
 while not resposta == 3:
+	print("#" * 30)
+	print("Bem vindo ao Banco Fox!")
+	print("#" * 30)
+	
 	resposta = int(input("1 - Operações de Gerente\n2 - Operações de Cliente\n3 - Sair"))
+	
+	
+	
+	print("=" * 50)
+	login = int(input("  Sistema Seguro - Acesso Restrito  \n  Login necessário para continuar:"))
+	if banco_de_dados.carregar_contas_de_csv(caminho_arquivo= "contas.csv"):
+			pass 
+ 
+	
+	
+	
+	#PARTE GERENTE
 	if resposta == 1:
 		print("#" * 40)
 		print("Modo de Gerente Acessado!\nEscolha uma operação:")
@@ -17,21 +30,63 @@ while not resposta == 3:
 						"4 - Debitar fundos em lote\n" \
 					"5 - Abrir uma nova conta no banco\n" \
 				"6 - Voltar ao menu principal "))
+		if resposta_gerente == 1:
+			operacoes_bancarias.somar_saldos_gerais()
 		
+		elif resposta_gerente == 2:
+			operacoes_bancarias.identificar_cliente_mais_rico()
+		
+		elif resposta_gerente == 3:
+			operacoes_bancarias.somar_saldos_em_lote()
+		
+		elif resposta_gerente == 4:
+			operacoes_bancarias.subtrair_saldos_em_lote()
+		
+		elif resposta_gerente == 5:
+			operacoes_bancarias.criar_conta()
+		
+		elif resposta_gerente == 6:
+			pass
+
 	
-	
+	#PARTE CLIENTE
 	elif resposta == 2:
-		print("#" * 40)
-		
-		
-		
+		print("#" * 40)	
 		print("Modo de Cliente acessado!!\nEscolha uma operação:")
 		resposta_cliente = int(input("1 - Consultar meu saldo\n" \
 							   "2 - Realizar um depósito\n" \
 						"3 - Realizar um saque\n" \
 					"4 - Realizar uma transferênci\n"
 				"5 - Voltar ao menu principal"))
-													
+		if resposta_cliente == 1:
+			operacoes_bancarias.consultar_saldo()
+
+		elif resposta_cliente == 2:
+			operacoes_bancarias.depositar(numero_conta, valor)
+			print("#" * 40)
+			print("  Setor de Deposito Acessado!!  ")
+			numero_conta = str(input("Digite o número de sua conta: "))
+		
+		elif resposta_cliente == 3:
+			print("#" * 40)
+			print("  Setor de Saque Acessado!!  ")
+			 
+			numero_conta = str(input("Digite o número de sua conta\n: "))
+			print(f"Conta {numero_conta} acessada com sucesso!")
+			valor = float(input("Digite o valor que deseja sacar:"))
+			print("~"* 5)
+			operacoes_bancarias.sacar(numero_conta, valor)
+			print(f"Você sacou {valor} de sua conta!")
+		
+		elif resposta_cliente == 4:
+			
+			conta_origem = str(input("Digite o número da conta de origem\n: "))
+			print("~"*5)
+			conta_destino = str(input("Agora digite o número da conta de destino\n:"))
+			operacoes_bancarias.realizar_transferencia(conta_origem, conta_destino, valor)
+
+		elif resposta_cliente == 5:
+			pass												
 
 	elif resposta == 3:
 		print("Encerrando")
@@ -41,15 +96,15 @@ while not resposta == 3:
 
 """
 funções:
-criar_conta
-depositar
-sacar
-consultar_saldo
-somar_saldos_gerais
-identificar_cliente_mais_rico
-somar_saldos_em_lote
-subtrair_saldos_em_lote
-realizar_transferencia
+criar_conta()
+depositar()
+sacar()
+consultar_saldo()
+somar_saldos_gerais()
+identificar_cliente_mais_rico()
+somar_saldos_em_lote()
+subtrair_saldos_em_lote()
+realizar_transferencia()
 
 
 
--- operacoes_bancarias.py
+++ operacoes_bancarias.py
@@ -6,7 +6,109 @@ O carregamento e salvamento do arquivo CSV devem gerenciados internamente pelas 
 passar o dicionário de contas como argumento. Para isso, vocês devem usar as funções do módulo banco_de_dados.py 
 para ler e salvar o CSV ao realizar as operações."""
 import banco_de_dados
+#########################################################################################
 
+"""
+CRIAR CONTA
+Descrição: Deve criar uma conta nova, incluindo no csv. 
+Note que essa conta deve começar com um saldo de 0 reais.
+
+Retorno: Uma tupla com o numero da nova conta criada e 
+o dicionário completo dela (ex: (0001, {"cliente": "Joao", "saldo": 0.00}))."""
 
 def criar_conta(numero_conta: str, nome_cliente: str) -> tuple[int, dict]:
     banco_de_dados.salvar_contas_para_csv(contas=(numero_conta, dict("nome_cliente", nome_cliente, "saldo", 0)))
+
+
+########################################################################################
+
+"""
+SOMAR SALDOS GERAIS
+Descrição: Soma o saldo de todas as contas existentes no banco.
+
+Retorno: O valor total (float) da soma de todos os saldos."""
+
+def somar_saldos_gerais( ) -> float:
+    pass
+
+
+#########################################################################################
+"""
+DEPOSITAR
+Descrição: Se a conta existir e o valor for positivo, adiciona o valor ao saldo.
+
+Retorno: Uma tupla indicando sucesso/falha com uma mensagem apropriada (ex: (False, "Erro: conta inexistente."))."""
+
+def depositar(numero_conta: str, valor: float) -> tuple[bool, str]:
+    pass
+
+
+#########################################################################################
+
+"""
+SACAR
+Descrição: Verifica se a conta existe, se o valor é positivo e se há saldo suficiente. Se tudo estiver OK, subtrai o valor do saldo.
+
+Retorno: Uma tupla indicando sucesso/falha com uma mensagem apropriada (ex: (False, "Erro: saldo insuficiente."))."""
+
+def sacar(numero_conta: str, valor: float) -> tuple[bool, str]:
+    pass
+
+
+###########################################################################################
+
+"""
+CONSULTAR SALDO
+Descrição: Procura pela conta no dicionário e retorna o saldo dela.
+
+Retorno: O saldo (um float) se a conta existir, ou None se não existir."""
+
+def consultar_saldo(numero_conta: str) -> float | None:
+    pass
+
+
+############################################################################################
+
+"""
+IDENTIFICAR CLIENTE MAIS RICO
+Descrição: Percorre todas as contas e identifica qual cliente possui o maior saldo.
+
+Retorno: O dicionário completo da conta com o maior saldo (ex: {"cliente": "Carlos", "saldo": 5000.00}). 
+Se não houver contas, retorna None."""
+
+def identificar_cliente_mais_rico() -> dict | None:
+    pass
+
+
+#############################################################################################
+
+"""
+SOMAR SALDOS EM LOTE
+Descrição: Soma valores aos saldos de múltiplas contas de uma vez. As chaves dos kwargs serão os 
+números das contas e os valores serão os montantes a serem adicionados.
+
+Retorno: A quantidade de contas que foram atualizadas com sucesso. 
+Contas inexistentes ou valores negativos devem ser ignorados."""
+
+
+def somar_saldos_em_lote(**kwargs) -> int:
+    pass
+
+##############################################################################################
+
+"""
+SUBTRAIR SALDOS EM LOTE
+Descrição: Subtrai valores dos saldos de múltiplas contas de uma vez. Falha para uma conta específica 
+se o saldo for insuficiente, mas continua para as outras.
+
+Retorno: A quantidade de contas que tiveram o saldo removido com sucesso. Contas inexistentes, 
+valores negativos ou tentativas de saque maiores que o saldo devem ser ignoradas."""
+
+def subtrair_saldos_em_lote(**kwargs) -> int:
+    pass
+
+#################################################################################################
+
+def realizar_transferencia():
+    pass
+
