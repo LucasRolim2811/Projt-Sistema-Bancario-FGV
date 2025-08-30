8193ef1b890fc2cf301ddb04c05091aec09eba52
Albuquerque07 <rafa.moraes2003@hotmail.com>
2025-08-30 13:23:22 -0300

    Banco de dados 100%
    Ainda tem função meio ruim

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -1,13 +1,6 @@
-"""Descrição: Lê o arquivo CSV e o carrega em um dicionário. Se o arquivo não existir, 
-deve retornar um dicionário vazio.
-
-
-Retorno: Um dicionário no formato {"numero_conta": {"cliente": "Nome", "saldo": 123.45}, ...}. 
-O saldo deve ser um float."""
-
 import csv
 
-def carregar_contas_de_csv(caminho_arquivo: str) -> dict:
+def carregar_contas_de_csv(caminho_arquivo: str = "contas.csv") -> dict:
 
     """ Faz o acesso ao arquivo csv e lê cada linha contida dentro dele
 
@@ -18,14 +11,22 @@ def carregar_contas_de_csv(caminho_arquivo: str) -> dict:
         dict: Retorna um dicionário possuindo as linhas contidas no arquivo csv dado em caminho_arquivo
     """
     try:
-        with open(caminho_arquivo, "r",encoding='utf-8') as arquivo_csv:
+        contas = {}
+        with open(caminho_arquivo, "r", encoding='utf-8', newline="") as arquivo_csv:
             acessando_arquivo = csv.DictReader(arquivo_csv)
-            for lines in acessando_arquivo:
-                print(lines)
-        return acessando_arquivo
-    
+            for linha in acessando_arquivo:
+                numero_conta = linha["numero_conta"]
+                linha["saldo"] = float(linha["saldo"]) #Transforma o saldo em float
+                contas[numero_conta] = {"nome_cliente": linha["nome_cliente"], "saldo": linha["saldo"]}
+        return contas
+
+
     except FileNotFoundError:
         print(f"Arquivo {caminho_arquivo} não encontrado. Por favor, digitar um caminho válido.")
+        return {}
+
+
+
 
 def salvar_contas_para_csv(caminho_arquivo:str="contas.csv", contas:dict=dict()) -> None:
 
@@ -36,11 +37,13 @@ def salvar_contas_para_csv(caminho_arquivo:str="contas.csv", contas:dict=dict())
         contas (dict): Dicionário que o usuário quer inserir no banco de dados do arquivo csv
     """
     try:
-        with open(caminho_arquivo, "w", encoding="utf-8", newline="") as arquivo_csv:
-            colunas = ["nome_cliente", "saldo"]
+        with open(caminho_arquivo, "a", encoding="utf-8", newline="") as arquivo_csv:
+            colunas = ["numero_conta", "nome_cliente", "saldo"]
             acessar_csv = csv.DictWriter(arquivo_csv, fieldnames=colunas)
-            acessar_csv.writeheader()
-            acessar_csv.writerow(contas)
+            if arquivo_csv.tell() == 0:
+                acessar_csv.writeheader()
+            for conta, dicionario in contas.items():
+                acessar_csv.writerow({"numero_conta": conta, **dicionario})
 
     except FileNotFoundError:
         print(f"O arquivo {caminho_arquivo} dado pelo usuário não foi encontrado.")
@@ -49,8 +52,4 @@ def salvar_contas_para_csv(caminho_arquivo:str="contas.csv", contas:dict=dict())
         print(f"Dicionário dicionado no banco de dados COM SUCESSO!!!")
 
 
-# Driver code
-
-
-carregar_contas_de_csv("contas.csv")
-salvar_contas_para_csv("contas.csv", {"nome_cliente": "Rafael", "saldo": 0})
+salvar_contas_para_csv(contas={"0001": {"nome_cliente":"rafael", "saldo": 1000}, "0002": {"nome_cliente":"lucas", "saldo": 10000},"0003": {"nome_cliente":"Débhora", "saldo": 2000}})
--- operacoes_bancarias.py
+++ operacoes_bancarias.py
@@ -1,14 +1,132 @@
-"""Este módulo deve conter as funções principais relacionadas às operações bancárias. É importante destacar 
-que você pode criar funções auxiliares para facilitar a implementação e manter o código organizado. Lembre-se de 
-seguir o princípio de responsabilidade única, garantindo que cada função tenha um propósito claro e específico, 
-tornando-a mais reutilizável.
-O carregamento e salvamento do arquivo CSV devem gerenciados internamente pelas funções, sem a necessidade de 
-passar o dicionário de contas como argumento. Para isso, vocês devem usar as funções do módulo banco_de_dados.py 
-para ler e salvar o CSV ao realizar as operações."""
 import banco_de_dados
 
+class Erro_No_Banco(Exception):
+    """Verifica se o numero da conta existe"""
+class Conta_Inexistente(Erro_No_Banco):
+    """Numero da conta(numero_conta) não existe no banco de dados"""
+class Saldo_Insuficiente(Erro_No_Banco):
+    """Saldo inferior ao requisitado. Impossível realizar operação"""
 
 def criar_conta(numero_conta: str, nome_cliente: str) -> tuple[int, dict]:
+    """Vai criar uma conta dentro do csv de contas.csv
+
+    Args:
+        numero_conta (str): Vai colocar o código da conta que se deseja criar
+        nome_cliente (str): Nome do cliente que fará a conta
+
+    Returns:
+        tuple[int, dict]: Retorna uma tupla com o numero da conta e um dicionário com o nome e saldo do cliente
+    """
+    dicionarios = banco_de_dados.carregar_contas_de_csv()
+    if numero_conta in dicionarios.keys():
+        print("Esse código já existe!!")
+    elif numero_conta not in dicionarios.keys():
+        banco_de_dados.salvar_contas_para_csv(contas={"numero_conta": numero_conta,"nome_cliente": nome_cliente, "saldo": 0}) #Insere os dados no csv
+        return (numero_conta, {"nome_cliente": nome_cliente, "saldo": 0})
     
-    banco_de_dados.salvar_contas_para_csv(contas=(numero_conta, dict("nome_cliente", nome_cliente, "saldo", 0)))
+
+def depositar(numero_conta: str, valor: float) -> tuple[bool, str]:
+    """ Deposita um uma valor dado no saldo da conta especificada
+
+    Args:
+        numero_conta (str): Código da conta que se deseja fazer o depósito
+        valor (float): Valor que se deseja depositar
+
+    Returns:
+        tuple[bool, str]: Retorna se foi possível fazer o depósito[True/False] e uma mensagem de feedback
+    """
+    dicionarios = banco_de_dados.carregar_contas_de_csv()
+    if numero_conta in dicionarios.keys():
+        print("-" * 30)
+        print("Sua conta existe!!")
+        dicionarios[numero_conta]["saldo"] = banco_de_dados.salvar_contas_para_csv(valor)
+    elif numero_conta not in dicionarios.keys():
+        print("Tentativa de depósito falhou. Por favor, digitar uma conta válida")
+
+
+def sacar(numero_conta: str, valor: float) -> tuple[bool, str]:
+    """ Faz um saque na conta especificada. Subtrai o valor diretamente do saldo da conta
+
+    Args:
+        numero_conta (str): Código da conta que se deseja fazer o saque
+        valor (float): Valor que se deseja sacar
+
+    Returns:
+        tuple[bool, str]: Retorna se foi possível fazer o depósito[True/False] e uma mensagem de feedback
+    """
+    dicionarios = banco_de_dados.carregar_contas_de_csv()
+    try:
+        saldo_disponivel = dicionarios[numero_conta]["saldo"]
+        saldo_disponivel = saldo_disponivel - valor
+        banco_de_dados.salvar_contas_para_csv(contas=dicionarios)
+
+        
+    except Conta_Inexistente:
+        print("Tal conta é inválida! Tente uma válida")
     
+    else:
+        print("deu bom")
+
+def consultar_saldo(numero_conta: str) -> float | None:
+    """Verifica o saldo da conta informada
+
+    Args:
+        numero_conta (str): Código da conta que será acessada
+
+    Returns:
+        float | None: Retorna o saldo(em float) se a conta existir ou None caso contrário
+    """
+    pass
+
+def somar_saldos_gerais( ) -> float:
+    """Soma todos os saldos das contas informadas
+
+    Returns:
+        float: Retorna a soma de todas as contas cadastradas
+    """
+    pass
+
+def identificar_cliente_mais_rico( ) -> dict | None:
+    """Verifica qual cliente possui maior saldo na conta
+
+    Returns:
+        dict | None: Retorna todos os dados do cliente com maior saldo. None caso não haja contas no banco
+    """
+    pass
+
+
+def somar_saldos_em_lote(**kwargs) -> int:
+    """Soma valores aos saldos de múltiplas contas de uma vez. As chaves dos kwargs serão os números das contas 
+    e os valores serão os montantes a serem adicionados.
+
+    Returns:
+        int: A quantidade de contas que foram atualizadas com sucesso. Contas inexistentes ou valores negativos 
+        devem ser ignorados
+    """
+    pass
+
+def subtrair_saldos_em_lote(**kwargs) -> int:
+    """Subtrai valores dos saldos de múltiplas contas de uma vez. Falha para uma conta específica se o saldo 
+    for insuficiente, mas continua para as outras.
+
+    Returns:
+        int: A quantidade de contas que tiveram o saldo removido com sucesso. Contas inexistentes,
+        valores negativos ou tentativas de saque maiores que o saldo devem ser ignoradas.
+    """
+    pass
+
+def realizar_transferencia(conta_origem: str, conta_destino: str, valor: float) -> tuple[bool, str]:
+    """Realiza uma transferência entre duas contas. Se não for possível realizar a operação, seja por 
+    falta de saldo ou pela outra conta não existir, o retorno deve indicar a falha.
+
+    Args:
+        conta_origem (str): Código da conta da onde deseja transferir
+        conta_destino (str): Código da conta que se deseja receber a transferência
+        valor (float): Valor que se deseja transferir
+
+    Returns:
+        tuple[bool, str]: Tupla (sucesso, mensagem). Ex: (True, "Transferência realizada com sucesso.") 
+        ou (False, "Saldo insuficiente").
+    """
+    pass
+
